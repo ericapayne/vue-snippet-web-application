cd96dc0e8b1dd27190d663d76ab1bb9604115a9f
erizilla <39068554+erizilla@users.noreply.github.com>
2024-04-12 09:43:30 -0700

    push 4.12

--- snippet-client/vite.config.js
+++ snippet-client/vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-snippet-web-application/",
+  base: '/vue-snippet-web-application/',
   plugins: [vue()],
 })
